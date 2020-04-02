4ac9878a6b272d8b4ef459c454a4b8b2d84c5773
Tim Kelly <tim.kelly@ert.com>
2020-04-02 10:11:38 -0400

    Android 10 updates
    - Updates to support Android 10 (device and SDK)
    - Change to androidx imports
    - Conditionally skip autoLaunch behavior on Android 10
    - Some bug fixes with channel naming and firing notifications.
    
    Firing Notifications if autoLaunch:
    - No longer fires notifications if autoLaunch is true.
    - In above case, trigger event should schedule an immediate notification
    - The intent is that it now can be conditional
    - Still fires notification immediately on Android 10 when app is closed

--- www/local-notification.js
+++ www/local-notification.js
@@ -93,9 +93,9 @@ exports.requestPermission = function (callback, scope) {
 /**
  * Check to see if the user has allowed "Do Not Disturb" permissions for this app.
  * This is required to use alarmVolume to take a user out of silent mode.
- * 
+ *
  * @param {Function} callback The function to be exec as the callback.
- * @param {Object} scope callback function's scope 
+ * @param {Object} scope callback function's scope
  */
 exports.hasDoNotDisturbPermissions = function (callback, scope) {
     this._exec('hasDoNotDisturbPermissions', null, callback, scope);
@@ -105,7 +105,7 @@ exports.hasDoNotDisturbPermissions = function (callback, scope) {
  * Request "Do Not Disturb" permissions for this app.
  * The only way to do this is to launch the global do not distrub settings for all apps.
  * This permission is required to use alarmVolume to take a user out of silent mode.
- * 
+ *
  * @param {Function} callback The function to be exec as the callback.
  * @param {Object} scope callback function's scope.
  */
@@ -137,6 +137,11 @@ exports.isIgnoringBatteryOptimizations = function (callback, scope) {
  * @param {Object} scope callback function's scope
  */
 exports.requestIgnoreBatteryOptimizations = function (callback, scope) {
+    if (device.platform === 'iOS') {
+        console.warn('[Notifications] requestIgnoreBatteryOptimizations not supported on iOS');
+        callback(true);
+    }
+
     this._exec('requestIgnoreBatteryOptimizations', null, callback, scope);
 }
 
