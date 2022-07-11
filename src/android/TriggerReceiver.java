3b76e25e7a0aea8e523a3314a11a9f61abe864c6
Marko Rajevic <me@markorajevic.space>
2022-07-11 21:47:34 +0200

    Update local trigger & click trigger

--- src/android/TriggerReceiver.java
+++ src/android/TriggerReceiver.java
@@ -41,6 +41,9 @@ import static de.appplant.cordova.plugin.localnotification.LocalNotification.fir
 import static de.appplant.cordova.plugin.localnotification.LocalNotification.isAppRunning;
 import static java.util.Calendar.MINUTE;
 
+import org.apache.cordova.firebase.FirebasePlugin;
+import org.json.JSONException;
+
 /**
  * The alarm receiver is triggered when a scheduled alarm is fired. This class
  * reads the information in the intent and displays this information in the
@@ -75,6 +78,11 @@ public class TriggerReceiver extends AbstractTriggerReceiver {
         notification.show();
 
         if (!isUpdate && isAppRunning()) {
+            try {
+                FirebasePlugin.sendMessage(notification.toBundle(), notification.getContext());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             fireEvent("trigger", notification);
         }
 
