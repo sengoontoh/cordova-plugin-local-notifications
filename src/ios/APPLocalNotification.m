06dadcdcd19bd755568d9bc2b6de4691308c5015
Bohdana <bohdana.prokopchuk@techmagic.co>
2023-04-03 19:17:33 +0300

    HB-3199 usebraze-swift-sdk instead of appboy-ios-sdk

--- src/ios/APPLocalNotification.m
+++ src/ios/APPLocalNotification.m
@@ -30,14 +30,8 @@
 #import "FirebasePlugin.h"
 
 #import <objc/runtime.h>
-#if __has_include(<Appboy_iOS_SDK/AppboyKit.h>)
-#import <Appboy_iOS_SDK/AppboyKit.h>
-#elif __has_include(<Appboy-iOS-SDK/Appboy_iOS_SDK.framework/Headers/AppboyKit.h>)
-#import <Appboy-iOS-SDK/Appboy_iOS_SDK.framework/Headers/AppboyKit.h>
-#else
-#import "AppboyKit.h"
-#endif
-#import "AppboyPlugin.h"
+@import BrazeKit;
+#import "BrazePlugin.h"
 
 @interface APPLocalNotification ()
 
@@ -530,7 +524,7 @@ UNNotificationPresentationOptions const OptionAlert = UNNotificationPresentation
                 withCompletionHandler:handler];
     
     if ([mutableUserInfo objectForKey:@"ab"]) {
-        [[Appboy sharedInstance] userNotificationCenter:center didReceiveNotificationResponse:response withCompletionHandler:nil];
+        [[Braze sharedInstance] userNotificationCenter:center didReceiveNotificationResponse:response withCompletionHandler:nil];
     }
 
     handler();
